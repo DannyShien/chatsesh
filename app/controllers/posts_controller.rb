4ed8e241b7c505302a0c6d4f67f5156fe6413b1c
“Stanley <dannyshien@gmail.com>
2017-08-17 17:48:35 +0700

    add post feature to page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,9 @@
 class PostsController < ApplicationController
+  def create 
+    a = Post.new
+    a.body = params[:post][:body]
+    a.poster_id = params[:post][:poster_id]
+    a.save
+    redirect_to root_path
+  end
 end
